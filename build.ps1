29db5ca628d04e881226e2af650ad72be9c8f6ee
Robert Labrie <robert.labrie@gmail.com>
2014-05-01 14:41:46 -0400

    add psm1 to build script

--- build.ps1
+++ build.ps1
@@ -4,4 +4,7 @@ Get-ChildItem ".\*" -Filter "OPSView*.ps1" | `
 ForEach-Object {
     $c = Get-Content $_.FullName
     $c | Out-File ".\build\OPSView.ps1" -Append
+    $c | Out-File ".\build\OPSView.psm1" -Append
 }
+
+"Export-ModuleMember -Function *" | Out-File ".\build\OPSView.psm1" -Append
