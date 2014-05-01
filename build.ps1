e7a3e670156d2138f7e0e2016ccd9a83ec6375c6
Robert Labrie <robert.labrie@gmail.com>
2014-05-01 14:36:57 -0400

    create build dir and script to roll all files into one

---
+++ build.ps1
@@ -0,0 +1,7 @@
+﻿if (Test-Path ".\build\OPSView*") { Remove-Item ".\build\OPSView*" }
+
+Get-ChildItem ".\*" -Filter "OPSView*.ps1" | `
+ForEach-Object {
+    $c = Get-Content $_.FullName
+    $c | Out-File ".\build\OPSView.ps1" -Append
+}
