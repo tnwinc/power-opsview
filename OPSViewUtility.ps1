7dbd315a7b4976a702adec378ea5013a13007569
Robert Labrie <robert.labrie@gmail.com>
2014-05-01 14:00:50 -0400

    added Get-OPSViewObjectType

--- OPSViewUtility.ps1
+++ OPSViewUtility.ps1
@@ -7,3 +7,28 @@
     $end = Get-date -date $timestamp
     return (New-TimeSpan -Start $start -end $end).TotalSeconds
 }
+
+function Get-OPSViewObjectType
+{
+    Param(
+        $OPSViewObject,
+        $is
+    )
+    if (!$OPSViewObject.ref) { return $false }
+    $result = $OPSViewObject.ref
+
+    $result = $result.replace("/rest/","")
+
+    #drop the trailing ID
+    $result = $result.substring(0,$result.LastIndexOf('/'))
+    
+    if ($is)
+    {
+        return ($result -eq $is)
+    }
+    else
+    {
+        return $result
+    }
+
+}
