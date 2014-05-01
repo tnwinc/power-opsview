86ec5bde06f290f1d849c14851df102950f6a30b
Robert Labrie <robert.labrie@gmail.com>
2014-05-01 08:36:44 -0400

    Moved function from OPSViewHost to OPSViewDowntime

--- OPSViewDowntime.ps1
+++ OPSViewDowntime.ps1
@@ -18,3 +18,21 @@
     $result = Execute-OPSViewRESTCall -service $service -verb "get" -OPSViewSession $OPSViewSession
     return $result.list
 }
+function Remove-OPSViewDowntime
+{
+    Param(
+        [Parameter(Mandatory=$True)]$OPSViewDownTime
+    )
+    write-host "-------------------"
+    write-host $OPSViewDownTime | ft
+    
+    echo OPSViewEpoch -timestamp $OPSViewDownTime.start_time
+    $service = '/rest/downtime'
+    $service += "h.name=IT-NETOPS"
+    #$service += '?start_time=' + (OPSViewEpoch -timestamp $OPSViewDownTime.start_time)
+    #$service += '&comment=' + [System.Web.HttpUtility]::UrlEncode($OPSViewDownTime.comment)
+
+    write-host $service
+    $result = Execute-OPSViewRESTCall -service $service -verb "delete"
+    write-host $result
+}
