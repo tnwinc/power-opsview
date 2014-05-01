be1e521107639cd0ab2d04c90c1f7c9da6d1ec38
Robert Labrie <robert.labrie@gmail.com>
2014-05-01 14:13:49 -0400

    Set-OPSViewContact

--- OPSViewContact.ps1
+++ OPSViewContact.ps1
@@ -15,3 +15,36 @@
     $result = Get-OPSViewconfig -objectType 'contact' -filter $filter -OPSViewSession $OPSViewSession
     return $result.list
 }
+
+function Set-OPSViewContact
+{
+    Param(
+        [Parameter(Mandatory=$True)]$OPSViewContact,
+        $language,
+        $name,
+        $variables,
+        $description,
+        $notificationprofiles,
+        $fullname,
+        $realm,
+        $role,
+        $uncomitted,
+        $OPSViewSession
+    )
+    if ($OPSViewContact -is [string]) { $OPSViewContact = Get-OPSViewContact -name $OPSViewContact }
+    if (!(Get-OPSViewObjectType -OPSViewObject $OPSViewContact -is 'config/contact'))
+    {
+        Throw "Expected string or OPSViewContact. Found $(Get-OPSViewObjectType -OPSViewObject $OPSViewContact)"
+    }
+
+    $service = $OPSViewContact.ref
+
+    $payload = $PSBoundParameters
+    $payload.Remove("OPSViewContact")
+    $payload.Remove("OPSViewSession")
+
+
+    $result = Execute-OPSViewRESTCall -verb 'put' -service $service -payload (ConvertTo-Json $payload) -OPSViewSession $OPSViewSession
+
+    return $result
+}
